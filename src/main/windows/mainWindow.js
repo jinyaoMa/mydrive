6552affe315f0b3ac6b3514b707b81cba08ab0b6
jinyaoMa <jinyao.ma@outlook.com>
2021-10-06 03:32:53 -0400

    .

--- src/main/windows/mainWindow.js
+++ src/main/windows/mainWindow.js
@@ -1,35 +1,35 @@
-import { BrowserWindow, Menu } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+import { BrowserWindow, Menu } from "electron";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 
 class MainWindow {
-  constructor (win) {
+  constructor(win) {
     /* win代表electron窗口实例
      win is this electron window instance */
-    this.win = win
+    this.win = win;
   }
 
-  initBrowserPage () {
+  initBrowserPage() {
     // Dev or not
     if (process.env.WEBPACK_DEV_SERVER_URL) {
-      this.win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+      this.win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
       /* 开发环境下自启动开发者工具
         start developer tools in the development environment */
-      this.win.webContents.openDevTools({ mode: 'detach' })
+      this.win.webContents.openDevTools({ mode: "detach" });
     } else {
-      createProtocol('app')
-      this.win.loadURL('app://./index.html')
+      createProtocol("app");
+      this.win.loadURL("app://./index.html");
     }
 
-    this.win.on('closed', () => {
-      this.win = null
-      console.log('windows closed')
-    })
+    this.win.on("closed", () => {
+      this.win = null;
+      console.log("windows closed");
+    });
   }
 
-  createWindow () {
+  createWindow() {
     if (this.win) {
-      console.warn('window is already exists!')
-      return
+      console.warn("window is already exists!");
+      return;
     }
 
     const win = new BrowserWindow({
@@ -47,35 +47,16 @@ class MainWindow {
       // eslint-disable-next-line no-undef
       icon: `${__static}/app.ico`,
       frame: false
-    })
+    });
 
-    this.win = win
-    this.initBrowserPage()
-    this.setWindowMenu()
+    this.win = win;
+    this.initBrowserPage();
+    this.setWindowMenu();
   }
 
-  setWindowMenu () {
-    if (process.platform === 'darwin') {
-      const template = [
-        {
-          label: 'vue-cli-electron-template',
-          submenu: [
-            {
-              role: 'about'
-            },
-            {
-              role: 'quit'
-            }
-          ]
-        }
-      ]
-      const menu = Menu.buildFromTemplate(template)
-      Menu.setApplicationMenu(menu)
-      return
-    }
-
-    Menu.setApplicationMenu(null)
+  setWindowMenu() {
+    Menu.setApplicationMenu(null);
   }
 }
 
-export default MainWindow
+export default MainWindow;
